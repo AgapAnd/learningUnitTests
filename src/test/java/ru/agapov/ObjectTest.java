a0612d5d3f1169192551adb7b999959ffc73f0fd
AgapAnd <braniganzepp@gmail.com>
2024-10-14 12:06:52 +0300

    changes after review
    change junit on 5 version and add some blank rowa

--- src/test/java/ru/agapov/ObjectTest.java
+++ src/test/java/ru/agapov/ObjectTest.java
@@ -1,17 +1,17 @@
 package ru.agapov;
 
-import org.junit.Test;
 
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Class fClass is intended for testing behavior of class Object
- */
+
 public class ObjectTest {
     @Test
     public void successfullyReturnsStringRepresentationOfItself() {
         Object object = new Object();
         String expectedStringRepresentation = "java.lang.Object@" + Integer.toHexString(object.hashCode());
+
+
         String objectStringRepresentation = object.toString();
 
 
