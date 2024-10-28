04ee70ac7298e8473d4a433e0b16096fc604d628
AgapAnd <braniganzepp@gmail.com>
2024-10-28 13:25:12 +0300

    change swapTest after review

---
+++ src/test/java/ru/agapov/tests/SwapValuesTest.java
@@ -0,0 +1,26 @@
+package ru.agapov.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SwapValuesTest {
+    @Test
+    public void successfullyReturnsSwappedValuesOfTwoVariablesUsingTemporaryVariable() {
+        int firstVar = 1;
+        int secondVar = 2;
+        int tempVar = 0;
+        int expectedValueOfFirstVariable = 2;
+        int expectedValueOfSecondVariable = 1;
+
+
+        tempVar = firstVar;
+        firstVar = secondVar;
+        secondVar = tempVar;
+
+
+        assertThat(firstVar, equalTo(expectedValueOfFirstVariable));
+        assertThat(secondVar, equalTo(expectedValueOfSecondVariable));
+    }
+}
